6a1ef4e9be9336e59709783d30b576a2233dd834
Metin Senturk <metinsenturk@me.com>
2021-12-29 22:57:51 -0500

    seperate settings

--- weatherapp/config.py
+++ weatherapp/config.py
@@ -1,7 +1,14 @@
 import os
 
+# mongo db settings
 MONGODB_HOST = os.environ.get('MONGODB_HOST')
 MONGODB_PORT = os.environ.get('MONGODB_PORT')
 MONGODB_USER = os.environ.get('MONGODB_USER')
 MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
+
+# open weather app key
 OPEN_WEATHER_API_KEY = os.environ.get('OPEN_WEATHER_API_KEY')
+
+# local app settings
+APP_TOPONYM = os.environ.get('APP_TOPONYM', 'Istanbul, TR')
+APP_PULL_INTERVAL = os.environ.get('APP_PULL_INTERVAL', 60 * 10)
