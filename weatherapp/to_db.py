6abbb7b9d47fc89058b3639ab0f2ddf96460a50a
Metin Senturk <metinsenturk@me.com>
2021-12-29 22:58:21 -0500

    add config keys and location data

--- weatherapp/to_db.py
+++ weatherapp/to_db.py
@@ -1,13 +1,18 @@
 import time
 
-from .consumer import get_weather_at_location, get_weather_history_at_location
+from .config import APP_PULL_INTERVAL, APP_TOPONYM
+from .consumer import (get_geocode, get_weather_at_location,
+                       get_weather_history_at_location)
 from .db import weather, weather_insert
 
 if __name__ == "__main__":
     """
     Entry.
     """
-    toponym = 'New York, US'
+    toponym = APP_TOPONYM
+    location_data = get_geocode(toponym)
+    print('Requested city {}, {} at GPS ({}, {}).'.format(
+        location_data.name, location_data.country, location_data.lat, location_data.lon))
     weather_data_list = get_weather_history_at_location(toponym)
     print('Pulling new:', len(weather_data_list))
 
@@ -16,12 +21,15 @@ if __name__ == "__main__":
         print('Inserted documents: ', len(inserted))
 
     try:
+        print("""Starting to pull in 10 min intervals. Please change 
+        the environment variable if you want it more often.
+        """)
         while True:
             weather_data = get_weather_at_location(toponym)
             print(str(weather_data).replace(
                 '<pyowm.weatherapi25.weather.', '').replace('>', ''))
             weather_insert(weather_data)
-            time.sleep(5)
+            time.sleep(APP_PULL_INTERVAL)
     except KeyboardInterrupt:
         print('Operation canceled.')
     except SystemExit:
